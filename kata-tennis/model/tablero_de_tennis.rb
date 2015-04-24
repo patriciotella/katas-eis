c3c520d497ca3d7deb91355bf0786a323c63ac15
patriciotella <patriciotella@gmail.com>
2015-04-24 00:11:15 -0300

    test primera anotacion y pharse para traducir cada punto a la numeracion del tennis

--- kata-tennis/model/tablero_de_tennis.rb
+++ kata-tennis/model/tablero_de_tennis.rb
@@ -18,4 +18,30 @@ class TableroDeTennis
     @point_counter
   end
 
+  def anotacion_jugador_uno
+    point_counter[0] = point_counter.first + 1
+  end
+
+  def pharse_tennis_score(point)
+    case point
+      when 0
+        pharse = 0
+      when 1
+        pharse= 15
+      when 2
+        pharse= 30
+      when 3
+        pharse= 40
+      else
+        pharse= "definir advantage"
+    end
+    pharse
+  end
+
+  def get_resultado
+    jugador_a = pharse_tennis_score point_counter[0]
+    jugador_b= pharse_tennis_score point_counter[1]
+    "#{jugador_a},#{jugador_b}"
+  end
+
 end
--- kata-tennis/spec/tablero_de_tennis_spec.rb
+++ kata-tennis/spec/tablero_de_tennis_spec.rb
@@ -3,11 +3,20 @@ require_relative '../model/tablero_de_tennis.rb'
 
 describe 'Tablero de Tennis' do
 
+  partido = TableroDeTennis.new
+
   it 'deberia retornar todos los marcadores en 0 cuando inicia un juego' do
 
-    partido = TableroDeTennis.new
     partido.set_counter.should == [0,0]
     partido.game_counter.should == [0,0]
     partido.point_counter.should == [0,0]
+  end
+
+  it 'cuando el jugador 1 anota el primer punto deberia mostrarse en el marcador' do
+
+    partido.anotacion_jugador_uno
+
+    partido.point_counter.should == [1,0]
+    partido.get_resultado.should == "15,0"
   end
 end
