f3d616d321a76a736444263b1c4b255ab71e23e9
patriciotella <patriciotella@gmail.com>
2015-05-01 19:11:40 -0300

    primer commit batalla_naval, proyecto vacio

---
+++ batalla_naval/features/step_definitions/ships_create_steps.rb
@@ -0,0 +1,15 @@
+Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I create a small ship in position "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^position "(.*?)" is not empty$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I create a large ship in position "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
--- build_all.sh
+++ build_all.sh
@@ -5,4 +5,6 @@ cd char_counter
 bundle exec rake
 cd ..
 cd tenis
+cd ..
+cd batalla_naval
 bundle exec rake
