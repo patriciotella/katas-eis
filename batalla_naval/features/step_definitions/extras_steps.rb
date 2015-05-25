5c6b4ab9763c6e713a93354486c174b1ddcad0f9
patriciotella <patriciotella@gmail.com>
2015-05-25 16:55:49 -0300

    separe los specs agregue el web_steps modifique archivos

--- batalla_naval/features/step_definitions/extras_steps.rb
+++ batalla_naval/features/step_definitions/extras_steps.rb
@@ -1,17 +1,29 @@
 #require_relative '../../app/models/battle_ship.rb'
+#require 'uri'
+#require 'cgi'
+#require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 
-#Given(/^I create a large ship in position "([^"]*)" :"([^"]*)"$/) do |x, y|
-#  begin
-#  @board.alocate_large_ship_in(x.to_i,y.to_i)
-#  rescue InvalidLocation => @exc
+#module WithinHelpers
+#  def with_scope(locator)
+#    locator ? within(locator) { yield } : yield
 #  end
+#end
+#World(WithinHelpers)
+
+#Given(/^I create a large ship in position "([^"]*)":"([^"]*)"$/) do |x, y|
+  #begin
+  #  fill_in(:xLargeCoord, :with => x)
+  #  fill_in(:yLargeCoord, :with => y)
+  #  click_button "add_large_ship"
+  #rescue InvalidLocation => @exc
+  #end
 #end
 
 #Then(/^it will raise an error "([^"]*)"$/) do |exception_message|
-#  expect(@exc.message).to eq(exception_message)
+ # expect(@exc.message).to eq(exception_message)
 #end
 
-#Given(/^I shoot to position "([^"]*)" :"([^"]*)"$/) do |x, y|
+#Given(/^I shoot to position "([^"]*)":"([^"]*)"$/) do |x, y|
 #  begin
 #    @board.shoot_position(x.to_i,y.to_i)
 #  rescue InvalidLocation => @exc
