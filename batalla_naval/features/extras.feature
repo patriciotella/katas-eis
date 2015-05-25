a3c6e5cc9d928b86a99b8e187ee77bd15997f660
patriciotella <patriciotella@gmail.com>
2015-05-25 12:09:14 -0300

    Kata batalla_naval_2 creando estructura

--- batalla_naval/features/extras.feature
+++ batalla_naval/features/extras.feature
@@ -1,14 +1,14 @@
-#Feature: Extras
+Feature: Extras
 
- # Background:
-  #  Given a board with dimensions "5" x "5"
-
-
-  #Scenario: Create a large ship in a invalid location
-  #  Given I create a large ship in position "3" :"5"
-  #  Then it will raise an error "location is out of board"
+  Background:
+    Given a board with dimensions "5" x "5"
 
 
-  #Scenario: Shooting in a invalid location
-  #  Given I shoot to position "3" :"5"
-  #  Then it will raise an error "location is out of board"
+  Scenario: Create a large ship in a invalid location
+    Given I create a large ship in position "3" :"5"
+    Then it will raise an error "location is out of board"
+
+
+  Scenario: Shooting in a invalid location
+    Given I shoot to position "3" :"5"
+    Then it will raise an error "location is out of board"
--- batalla_naval/features/step_definitions/ships_create_steps.rb
+++ batalla_naval/features/step_definitions/ships_create_steps.rb
@@ -1,48 +1,17 @@
 require_relative '../../../batalla_naval/app/models/battle_ship.rb'
-require 'uri'
-require 'cgi'
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
-
-module WithinHelpers
-  def with_scope(locator)
-    locator ? within(locator) { yield } : yield
-  end
-end
-World(WithinHelpers)
-
 
 Given(/^a board with dimensions "(\d+)" x "(\d+)"$/) do |x, y|
-  visit '/mipagina'
-  fill_in 'xBoard' ,with: x
-  fill_in 'yBoard' ,with: y
-  find('#inciar_partida').click
-  #click_button "inciar_partida"
-  #@board = Board.new(x.to_i,y.to_i)
+  @board = Board.new(x.to_i,y.to_i)
 end
 
 Given(/^I create a small ship in position "([^"]*)" :"([^"]*)"$/) do |x, y|
-  fill_in 'xSmallCoord' , with:x
-  fill_in 'ySmallCoord' , with:y
-  #find('#add_small_ship').click
-  click_button "add_small_ship"
-  #fill_in(:xCoord, :with => x)
-  #fill_in(:yCoord, :with => y)
-  #click_button "inspect_coord"
-  #@board.alocate_small_ship_in(x.to_i,y.to_i)
+  @board.alocate_small_ship_in(x.to_i,y.to_i)
 end
 
 Then(/^position "([^"]*)":"([^"]*)" is not empty$/) do |x, y|
-  fill_in(:xCoord, :with => x)
-  fill_in(:yCoord, :with => y)
-  click_button "inspect_coord"
-  expect(page.has_content?(:coord_result)).should_not eq "water"
-  expect(page.has_content?(y)).to eq true
   expect(@board.empty_position(x.to_i,y.to_i)).to eq(false)
 end
 
 Given(/^I create a large ship in position "([^"]*)":"([^"]*)"$/) do |x, y|
-  fill_in(:xLargeCoord, :with => x)
-  fill_in(:yLargeCoord, :with => y)
-  click_button "add_large_ship"
-  #@board.alocate_large_ship_in(x.to_i,y.to_i)
+  @board.alocate_large_ship_in(x.to_i,y.to_i)
 end
